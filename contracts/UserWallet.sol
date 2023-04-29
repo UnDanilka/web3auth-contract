f4d06826b5e8671f69fb220dffb5ab59bae2b232
Danil Elfimov <dancingdanil98@gmail.com>
2023-04-29 14:20:42 +0300

    added userwallet

---
+++ contracts/UserWallet.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface SomeToken {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+}
+
+
+contract UserWallet  {
+    address public owner;
+
+
+    constructor() payable {
+        owner = msg.sender;
+    }
+
+    mapping(uint => bool) nonces;
+
+
+    receive() external payable {}
+
+    function withdrawEth(uint256 amount, uint256 nonce, bytes memory signature) external {
+        // проверяем использованный платеж или нет
+        require(!nonces[nonce], "nonce already used!");
+        // устанвливаем платеж использованным
+        nonces[nonce] = true;
+        // создаем сообщение на стороне контракта
+        bytes32 message = withPrefix(keccak256(abi.encodePacked(
+            msg.sender,
+            amount,
+            nonce,
+            address(this)
+        )));
+        // проверяем подписано ли сообщение владельцем 
+        require(
+            recoverSigner(message, signature) == owner, "invalid signature!"
+        );
+        // совершаем трансфер
+        payable(msg.sender).transfer(amount);
+    }
+
+    function withdrawToken(SomeToken token, uint256 amount, uint256 nonce, bytes memory signature) external {
+        require(!nonces[nonce], "nonce already used!");
+        // устанвливаем платеж использованным
+        nonces[nonce] = true;
+        // создаем сообщение на стороне контракта
+        bytes32 message = withPrefix(keccak256(abi.encodePacked(
+            msg.sender,
+            amount,
+            nonce,
+            address(this)
+        )));
+        // проверяем подписано ли сообщение владельцем 
+        require(
+            recoverSigner(message, signature) == owner, "invalid signature!"
+        );
+
+        uint256 tokenBalance = token.balanceOf(address(this));
+        require(amount <= tokenBalance, "balance is low");
+        token.transfer(msg.sender, amount);
+    }
+
+    // возвращает того, кто подписал сообщение
+    function recoverSigner(bytes32 message, bytes memory signature) private pure returns(address) {
+        (uint8 v, bytes32 r, bytes32 s) = splitSignature(signature);
+
+        return ecrecover(message, v, r, s);
+    }
+
+    // разбивает сообщение на r s v
+    function splitSignature(bytes memory signature) private pure returns(uint8 v, bytes32 r, bytes32 s) {
+        require(signature.length == 65);
+
+        assembly {
+            r := mload(add(signature, 32))
+
+            s := mload(add(signature, 64))
+
+            v := byte(0, mload(add(signature, 96)))
+        }
+
+        return(v, r, s);
+    }
+
+
+    // добавляет сообщению в смарт контракте необходимый префикс
+     function withPrefix(bytes32 _hash) private pure returns(bytes32) {
+        return keccak256(
+            abi.encodePacked(
+                "\x19Ethereum Signed Message:\n32",
+                _hash
+            )
+        );
+    }
+
+ 
+}
---
+++ test/UserWallet.js
@@ -0,0 +1,51 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+
+
+describe('UserWallet', function () {
+    let owner;
+    let receiver;
+    let userWallet;
+
+    beforeEach(async function () {
+        [owner, receiver] = await ethers.getSigners();
+        const UserWallet = await ethers.getContractFactory("UserWallet", owner);
+        userWallet = await UserWallet.deploy({ value: ethers.utils.parseUnits("100", 'ether') });
+        await userWallet.deployed();
+    });
+
+    it('should allow to recive and send payments', async () => {
+        const amount = ethers.utils.parseUnits("2", 'ether')
+        const nonce = 1
+
+        const hash = ethers.utils.solidityKeccak256(["address", "uint256", "uint256", "address"],
+            [receiver.address, amount, nonce, userWallet.address])
+
+
+        console.log('hash', hash)
+
+        const messageHashBin = ethers.utils.arrayify(hash)
+
+        console.log('messageHashBin', messageHashBin)
+
+        const signature = await owner.signMessage(messageHashBin)
+
+        console.log('signature', signature)
+
+        console.log('contract balance start', ethers.utils.formatEther(await ethers.provider.getBalance(userWallet.address)))
+        console.log('owner balance start', ethers.utils.formatEther(await ethers.provider.getBalance(owner.address)))
+        console.log('receiver balance start', ethers.utils.formatEther(await ethers.provider.getBalance(receiver.address)))
+
+        const tx = await userWallet.connect(receiver).withdrawEth(amount, nonce, signature)
+        tx.wait();
+
+        await expect(tx).to.changeEtherBalance(receiver, amount)
+
+        console.log('contract balance end', ethers.utils.formatEther(await ethers.provider.getBalance(userWallet.address)))
+        console.log('owner balance end', ethers.utils.formatEther(await ethers.provider.getBalance(owner.address)))
+        console.log('receiver balance end', ethers.utils.formatEther(await ethers.provider.getBalance(receiver.address)))
+
+
+
+    })
+})
